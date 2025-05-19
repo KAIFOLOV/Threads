1ff1e6434b4885acc1176f43a5a77037b881ca97
Шапоренко Алексей <shaporenko.as@micran.ru>
2025-05-19 18:53:05 +0700

    lab 1

---
+++ lab1.cpp
@@ -0,0 +1,38 @@
+#include <QCoreApplication>
+#include <iostream>
+#include <mutex>
+#include <thread>
+#include <QDebug>
+
+std::mutex mutex;
+
+void countdown(const int n)
+{
+    for (int i = n; i >= 0; --i) {
+        {
+            std::lock_guard<std::mutex> lock(mutex);
+            std::cout << "Thread ID: " << std::this_thread::get_id() << " - " << i << std::endl;
+        }
+
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        std::cout << "Thread ID: " << std::this_thread::get_id() << " finished" << std::endl;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    std::thread t1(countdown, 10);
+    std::thread t2(countdown, 20);
+
+    t1.join();
+    t2.join();
+
+    qInfo() << "All threads finished";
+
+    return a.exec();
+}
